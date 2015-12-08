3a862bc8091a05682a4f4d805855277129a3f55d
cryptedp <samipuran@gmail.com>
2015-12-08 20:16:33 +0100

    progress in assignment  8

--- Assignment8/Party.hs
+++ Assignment8/Party.hs
@@ -11,4 +11,14 @@ glcons e (GL elist fun) = GL (elist:e) empFun e + fun
 
 instance Monoid GuestList where
 mempty = GL [] 0
-mappend = (GL elist1 fun1) (GL elist2 fun2) = GL (elist1++elist2) (fun1+fun2)
+mappend (GL elist1 fun1) (GL elist2 fun2) = GL (elist1++elist2) (fun1+fun2)
+
+
+moreFun:: GuestList->GuestList->GuestList
+morefun a@(GL elist1 fun1) b@((GL elist2 fun2)
+	|fun1>fun2 = a
+	|otherwise = b
+	
+treeFold:: (a->[b]->b)->Tree a->b
+treeFold f init (Node{rootlabel=r,subForest=sf})= f r (map (treeFold f init) sf
+	
--- Streams/Stream.hs
+++ Streams/Stream.hs
@@ -9,3 +9,11 @@ show (Cons x s ) = show x ++ ","++( concat.map(\s->show s++",").take 30.streamTo
 streamrepeat:: a-> Stream a
 streamrepeat a = Cons a $ streamrepeat a
 
+streamMap:: (a->b)->Stream a->Stream b
+streamMap f Cons a s =Cons(f a) (streamMap f s)
+
+streamFromSeed:: (a->a) -> a -> Stream a
+streamFromSeed f seed = Cons(seed)(streamFromSeed f(f seed))
+
+nats:: Stream Integer
+nats = streamFromSeed (\x-> x+1)0
